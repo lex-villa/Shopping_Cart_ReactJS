6a645e2e91d1fc38bc3d0f2c2cdc61384273eed2
Alejandro VIlla <alexandros17.sk@gmail.com>
2021-01-14 18:38:49 -0600

    very simple validation form added

--- src/components/Form/CreditCardData/CreditCardData.js
+++ src/components/Form/CreditCardData/CreditCardData.js
@@ -6,6 +6,15 @@ import './CreditCardData.css';
 const CreditCardData = () => {
     const [creditCardQuery, setCreditCardQuery] = useState("");
 
+    const [inputName, setInputName] = useState(false);
+    const [inputExpDate, setInputExpDate] = useState(false);
+    const [inputCVV, setInputCVV] = useState(false);
+    const [inputCreditCardNumber, setInputCreditCardNumber] = useState(false);
+
+    const [creditCardDataFormIsValid, setCreditCardDataFormIsValid] = useState(false);
+
+    console.log('el form creditcicio es valido?', creditCardDataFormIsValid)
+
     const creditCardValidation = (ccQuery) => {
         fetch(`http://localhost:8080/validate/creditcard/${ccQuery}`, {
             method: 'POST',
@@ -17,8 +26,11 @@ const CreditCardData = () => {
                 return response.json();
             })
             .then(responseJson => {
-                console.log('soy la respuesta de la validacion credicticia')
-                console.log(responseJson)
+                if (responseJson.message === 'success') {
+                    setInputCreditCardNumber(true);
+                } else {
+                    setInputCreditCardNumber(false);
+                };
             })
     };
 
@@ -26,7 +38,48 @@ const CreditCardData = () => {
         const timeOutId = setTimeout(() => creditCardValidation(creditCardQuery), 1500);
 
         return () => clearTimeout(timeOutId);
-    }, [creditCardQuery])
+    }, [creditCardQuery]);
+
+    useEffect(()=> {
+        const checkValidityCreditCardDataForm = () => {
+            if (inputName && inputExpDate && inputCVV && inputCreditCardNumber) {
+                setCreditCardDataFormIsValid(true);
+            } else {
+                setCreditCardDataFormIsValid(false);
+            };
+        };
+        checkValidityCreditCardDataForm();
+    },[inputName, inputExpDate, inputCVV, inputCreditCardNumber]);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        if (name === 'Name') {
+            if (value.length > 0) {
+                setInputName(true);
+            } else {
+                setInputName(false);
+            };
+        };
+
+        if (name === 'ExpDate') {
+            if (value.length > 0) {
+                setInputExpDate(true);
+            } else {
+                setInputExpDate(false);
+            };
+        };
+
+        if (name === 'CVV') {
+            if (value.length > 0) {
+                setInputCVV(true);
+            } else {
+                setInputCVV(false);
+            };
+        };
+
+    };
 
     return (
         <div className='CreditCardData_Container'>
@@ -43,18 +96,18 @@ const CreditCardData = () => {
                 <div className='Row_CreditCardData'>
                     <div className='InputGroup_CreditCardData'>
                         <label>Full Name</label>
-                        <input type='text' />
+                        <input type='text' name='Name' onChange={handleChange} />
                     </div>
                 </div>
 
                 <div className='Row_CreditCardData'>
                     <div className='InputGroup_CreditCardData'>
                         <label>Exp. Date</label>
-                        <input type='text' />
+                        <input type='text' name='ExpDate' onChange={handleChange} />
                     </div>
                     <div className='InputGroup_CreditCardData'>
                         <label>CVV</label>
-                        <input type='text' />
+                        <input type='text' name='CVV' onChange={handleChange} />
                     </div>
                 </div>
             </div>
--- src/components/Form/ShippingData/ShippingData.js
+++ src/components/Form/ShippingData/ShippingData.js
@@ -5,6 +5,18 @@ import './ShippingData.css'
 const ShippingData = () => {
     const [ziCodeQuery, setZipCodeQuery] = useState("");
 
+    const [inputName, setInputName] = useState(false);
+    const [inputLastName, setInputLastName] = useState(false);
+    const [inputAdress, setInputAdress] = useState(false);
+    const [inputCity, setInputCity] = useState(false);
+    const [inputState, setInputState] = useState(false);
+    const [inputPhoneNumber, setInputPhoneNumber] = useState(false);
+    const [inputZipCode, setInputZipCode] = useState(false)
+
+    const [shippingDataFormIsValid, setShippingDataFormIsValid] = useState(false);
+
+    console.log('el form es valido?', shippingDataFormIsValid)
+
     const ziCodeValidation = (zcQuery) => {
         fetch(`http://localhost:8080/validate/zipcode/${zcQuery}`, {
             method: 'POST',
@@ -16,18 +28,85 @@ const ShippingData = () => {
                 return response.json();
             })
             .then(responseJson => {
-                console.log('soy la respuesta de la validacion')
-                console.log(responseJson)
+                if (responseJson.message === 'success') {
+                    setInputZipCode(true);
+                } else {
+                    setInputZipCode(false);
+                };
             })
+            .catch(error => console.log(error));
     };
-
 
     useEffect(() => {
         const timeOutId = setTimeout(() => ziCodeValidation(ziCodeQuery), 1500);
 
         return () => clearTimeout(timeOutId);
-    }, [ziCodeQuery])
+    }, [ziCodeQuery]);
 
+    useEffect(()=> {
+        const checkValidityShippingDataForm = () => {
+            console.log(inputName , inputLastName , inputAdress , inputCity , inputState , inputPhoneNumber , inputZipCode)
+            if (inputName && inputLastName && inputAdress && inputCity && inputState && inputPhoneNumber && inputZipCode) {
+                setShippingDataFormIsValid(true);
+            } else {
+                setShippingDataFormIsValid(false);
+            };
+        };
+        checkValidityShippingDataForm();
+    }, [inputName , inputLastName , inputAdress , inputCity , inputState , inputPhoneNumber , inputZipCode])
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        if (name === 'Name') {
+            if (value.length > 0) {
+                setInputName(true);
+            } else {
+                setInputName(false);
+            };
+        };
+
+        if (name === 'LastName') {
+            if (value.length > 0) {
+                setInputLastName(true);
+            } else {
+                setInputLastName(false);
+            };
+        };
+
+        if (name === 'Adress') {
+            if (value.length > 0) {
+                setInputAdress(true);
+            } else {
+                setInputAdress(false);
+            };
+        };
+
+        if (name === 'City') {
+            if (value.length > 0) {
+                setInputCity(true);
+            } else {
+                setInputCity(false);
+            };
+        };
+
+        if (name === 'State') {
+            if (value.length > 0) {
+                setInputState(true);
+            } else {
+                setInputState(false);
+            };
+        };
+
+        if (name === 'PhoneNumber') {
+            if (value.length > 0) {
+                setInputPhoneNumber(true);
+            } else {
+                setInputPhoneNumber(false);
+            };
+        };
+    };
 
     return (
         <div className='ShippingDataContainer'>
@@ -37,29 +116,29 @@ const ShippingData = () => {
                 <div className='Row_ShippingData'>
                     <div className='InputGroup_ShippingData'>
                         <label>Name</label>
-                        <input type='text' />
+                        <input type='text' name='Name' onChange={handleChange} />
                     </div>
                     <div className='InputGroup_ShippingData'>
                         <label>Last Name</label>
-                        <input type='text' />
+                        <input type='text' name='LastName' onChange={handleChange} />
                     </div>
                 </div>
 
                 <div className='Row_ShippingData'>
                     <div className='InputGroup_ShippingData'>
                         <label>Adress</label>
-                        <input type='text' />
+                        <input type='text' name='Adress' onChange={handleChange} />
                     </div>
                     <div className='InputGroup_ShippingData'>
                         <label>City</label>
-                        <input type='text' />
+                        <input type='text' name='City' onChange={handleChange} />
                     </div>
                 </div>
 
                 <div className='Row_ShippingData'>
                     <div className='InputGroup_ShippingData'>
                         <label>State</label>
-                        <input type='text' />
+                        <input type='text' name='State' onChange={handleChange} />
                     </div>
                     <div className='InputGroup_ShippingData'>
                         <label>Zip Code</label>
@@ -70,7 +149,7 @@ const ShippingData = () => {
                 <div className='Row_ShippingData'>
                     <div className='InputGroup_ShippingData'>
                         <label>Phone Number</label>
-                        <input type='text' />
+                        <input type='text' name='PhoneNumber' onChange={handleChange} />
                     </div>
                 </div>
             </div>
